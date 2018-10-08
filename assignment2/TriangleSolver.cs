d78313e76873f52cbe42e21617536e4869370f68
Eddie <edwaldojunior@gmail.com>
2018-10-07 21:22:04 -0400

    commit #2: Tests concluded.

---
+++ assignment2/TriangleSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    static public class TriangleSolver
+    {
+        static public string Analyze(int a, int b, int c)
+        {
+            
+        }
+    }
+}
