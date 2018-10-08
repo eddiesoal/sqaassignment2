c4f127db7691933e1254f38d899b9ad15f9fc8ff
Eddie <edwaldojunior@gmail.com>
2018-10-07 22:12:09 -0400

    Code of Triangle Solver implemented and NUnit configured and runned.

--- assignment2/TriangleSolver.cs
+++ assignment2/TriangleSolver.cs
@@ -10,7 +10,43 @@ namespace assignment2
     {
         static public string Analyze(int a, int b, int c)
         {
-            
+            // check if it is a triangle
+            if ((a + b < c) || (b + c < a) || (a + c < b))
+            {
+                return "NotATriangle";
+            }
+
+            // check if it is equilateral or isosceles
+            // considering the 3 variations
+            if (a == b)
+            {
+                if (a == c)
+                {
+                    return "equilateral";
+                }
+                return "isosceles";
+            }
+
+            if (a == c)
+            {
+                if (a == b)
+                {
+                    return "equilateral";
+                }
+                return "isosceles";
+            }
+
+            if (b == c)
+            {
+                if (b == a)
+                {
+                    return "equilateral";
+                }
+                return "isosceles";
+            }
+
+            // if none of above, it's scalene
+            return "scalene";
         }
     }
 }
